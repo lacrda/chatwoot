dfd744ab58d4dc5c72c4caf6e8f912ad3d562dbf
Guilherme Lacerda <lacerda@poli.ufrj.br>
2026-02-26 16:46:27 -0300

    fiz webhook queue

--- docker/start.sh
+++ docker/start.sh
@@ -7,7 +7,7 @@ mkdir -p tmp/pids tmp/cache
 bundle exec rails db:prepare
 
 if [ "$role" = "worker" ]; then
-  exec bundle exec sidekiq -C config/sidekiq.yml -q critical -q high -q default -q low
+  exec bundle exec sidekiq -C config/sidekiq.yml -q critical -q high -q medium -q default -q low
 fi
 
 exec bundle exec puma -C config/puma.rb -p "${PORT:-3000}"
